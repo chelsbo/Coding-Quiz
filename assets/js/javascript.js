f98a54377d0c75005faf38a5c2390286ffbb9529
chelsbo <chelsea.reese@icloud.com>
2022-08-10 20:45:26 -0600

    Updated JS, still making changes

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,6 +1,6 @@
 let startBtn = document.getElementById('start-quiz')
-startBtn.onclick = myFunction;
-// startBtn.addEventListener("click", run);
+// startBtn.onclick = myFunction;
+startBtn.addEventListener("click", generateQuiz);
 
 // startBtn.onclick = myFunction;
 
@@ -19,31 +19,31 @@ function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
 	showQuestions(questions, quizContainer);
 
 	// when user clicks submit, show results
-	submitButton.onclick = function(){
+	// submitButton.click = function(){
 		showResults(questions, quizContainer, resultsContainer);
 	}
-}
+// }
 
-// var myQuestions = [
-// 	{
-// 		question: "What is 10/2?",
-// 		answers: {
-// 			a: '3',
-// 			b: '5',
-// 			c: '115'
-// 		},
-// 		correctAnswer: 'b'
-// 	},
-// 	{
-// 		question: "What is 30/3?",
-// 		answers: {
-// 			a: '3',
-// 			b: '5',
-// 			c: '10'
-// 		},
-// 		correctAnswer: 'c'
-// 	}
-// ];
+var myQuestions = [
+	{
+		question: "What is 10/2?",
+		answers: {
+			a: '3',
+			b: '5',
+			c: '115'
+		},
+		correctAnswer: 'b'
+	},
+	{
+		question: "What is 30/3?",
+		answers: {
+			a: '3',
+			b: '5',
+			c: '10'
+		},
+		correctAnswer: 'c'
+	}
+];
 
 // function showQuestions(questions, quizContainer){
 // 	// we'll need a place to store the output and the answer choices
